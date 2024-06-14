321255b152ca2d4b27e840240d13131a027a7a80
Lewis Dick <lewis.sdick@gmail.com>
2024-06-14 18:37:46 +0100

    Formatting

--- cameo-viewer-api/ScheduledMoviesResponse.ts
+++ cameo-viewer-api/ScheduledMoviesResponse.ts
@@ -1,47 +1,47 @@
 interface ScheduledMoviesResponse {
-    response: string
-    movies: Movie[]
-  }
-  
-  interface Movie {
-    ID: string
-    ScheduledFilmId: string
-    Title: string
-    CinemaId: string
-    TrailerUrl: string
-    show_times: Showtime[]
-    available_cinemas: string[]
-    image_url: string
-  }
-  
-  interface Attribute {
-                  id: number
-                  attribute: string
-                  attribute_full: string
-                  description: string
-                  attribute_category: string
-                  display_type: number
-                  is_active: number
-                  created_at: string
-                  updated_at: string
-  }
-  
-  interface Showtime {
-    CinemaId: string
-    ScheduledFilmId: string
-    Showtime: string
-    EventId: string
-    SessionId: string
-    SessionAttributesNames: string[]
-    ScreenName: string
-    SoldoutStatus: number
-    GlobalEventId: string
-    cinema_image_url: string
-    movie_image_url: string
-    time_format: string
-    time: string
-    date: string
-    date_f: string
-    show_time: Date
-    attributes: Attribute[]
-  }
+  response: string;
+  movies: Movie[];
+}
+
+interface Movie {
+  ID: string;
+  ScheduledFilmId: string;
+  Title: string;
+  CinemaId: string;
+  TrailerUrl: string;
+  show_times: Showtime[];
+  available_cinemas: string[];
+  image_url: string;
+}
+
+interface Attribute {
+  id: number;
+  attribute: string;
+  attribute_full: string;
+  description: string;
+  attribute_category: string;
+  display_type: number;
+  is_active: number;
+  created_at: string;
+  updated_at: string;
+}
+
+interface Showtime {
+  CinemaId: string;
+  ScheduledFilmId: string;
+  Showtime: string;
+  EventId: string;
+  SessionId: string;
+  SessionAttributesNames: string[];
+  ScreenName: string;
+  SoldoutStatus: number;
+  GlobalEventId: string;
+  cinema_image_url: string;
+  movie_image_url: string;
+  time_format: string;
+  time: string;
+  date: string;
+  date_f: string;
+  show_time: Date;
+  attributes: Attribute[];
+}
--- cameo-viewer-api/index.ts
+++ cameo-viewer-api/index.ts
@@ -1,34 +1,41 @@
-import fastify from 'fastify'
-import ky from 'ky'
-import cors from '@fastify/cors'
+import fastify from "fastify";
+import ky from "ky";
+import cors from "@fastify/cors";
 
-const server = fastify()
+const server = fastify();
 
-const CameoID = "005"
+const CameoID = "005";
 
 function isAvailableAtCameo(movie: Movie): boolean {
-  return movie.available_cinemas.includes(CameoID)
+  return movie.available_cinemas.includes(CameoID);
 }
-    
+
 function filterCameoShowtimes(showtime: Showtime): boolean {
-  return showtime.CinemaId === CameoID
+  return showtime.CinemaId === CameoID;
 }
 
-server.register(cors, {})
+server.register(cors, {});
 
-server.get('/movies', async (request, reply): Promise<Movie[]> => {
-  const json: ScheduledMoviesResponse = await ky.post('https://www.picturehouses.com/api/scheduled-movies-ajax').json()
-  return reply.send(json.movies
-    .filter(m => isAvailableAtCameo(m))
-    .map(m => { 
-    return { ...m, show_times: m.show_times.filter(s => filterCameoShowtimes(s))}
-  }))
-})
+server.get("/movies", async (request, reply): Promise<Movie[]> => {
+  const json: ScheduledMoviesResponse = await ky
+    .post("https://www.picturehouses.com/api/scheduled-movies-ajax")
+    .json();
+  return reply.send(
+    json.movies
+      .filter((m) => isAvailableAtCameo(m))
+      .map((m) => {
+        return {
+          ...m,
+          show_times: m.show_times.filter((s) => filterCameoShowtimes(s)),
+        };
+      }),
+  );
+});
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
